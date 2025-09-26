3539afbadec9b4397535f75f5b85780285b67ea5
orlando <orlando.cogo@ynov.com>
2025-09-26 15:40:12 +0200

    feat(gitavancer): add the fonction getTens

---
+++ gitavancer.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func FiltrerPairs(nombres []int) []int {
+	var pairs []int
+	for _, nombre := range nombres {
+		if nombre%2 == 0 {
+			pairs = append(pairs, nombre)
+		}
+	}
+	return pairs
+}
+
+func main() {
+	liste := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	pairs := FiltrerPairs(liste)
+	fmt.Println("Nombres pairs :", pairs)
+}
+
+func getTens() []int {
+	var tens []int
+	for i := 10; i <= 100; i += 10 {
+		tens = append(tens, i)
+	}
+	return tens
+}
+
+func main() {
+	tens := getTens()
+	fmt.Println("Les dizaines de 1 à 100 :", tens)
+}
